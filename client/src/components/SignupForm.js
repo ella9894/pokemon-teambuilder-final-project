eff4601954e6f0ea2c6166070f3f06713c6bd900
OwlBag <Owlbag0410@gmail.com>
2022-08-10 17:33:12 -0400

    signup fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -25,7 +25,7 @@ const SignupForm = () => {
 
       console.log(data);
 
-      Auth.login(data.login.token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
     }
